4e99f5c7da58146eedd3042a7adc343b92a3bbed
Vishesh Singh <VisheshSingh16@gmail.com>
2021-01-23 11:25:53 -0600

    header and base card comps

---
+++ src/components/UI/BaseCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BaseCard'
+};
+</script>
+
+<style scoped>
+div {
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  box-shadow: 1px 3px 5px rgba(0, 0, 0, 0.2);
+  margin: 20px auto;
+  padding: 20px;
+  max-width: 40rem;
+}
+</style>
---
+++ src/components/layouts/TheHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'TheHeader',
+  props: ['title']
+};
+</script>
+
+<style scoped>
+header {
+  width: 100%;
+  background: #640032;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 5rem;
+}
+
+header h1 {
+  color: #fff;
+  margin: 0;
+}
+</style>
